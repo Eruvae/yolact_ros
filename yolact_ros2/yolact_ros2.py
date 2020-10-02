b3ad40ddfdbb17e677edc0f7078762f4800ac969
cgrain <coengruijt@gmail.com>
2020-10-02 16:26:28 +0200

    Make it possible to change where YolactNode publishes
    Some users might want to configure where a yolact node publishes. The best way to achieve that is by making it possible to change the namespace both topics are in. Right now, it always is `/yolact_ros2`. That is great as the default, but it would be better suited to make it possible to configure that. It would make it possible to easily create multiple yolact nodes, for example.

--- yolact_ros2/yolact_ros2.py
+++ yolact_ros2/yolact_ros2.py
@@ -39,8 +39,7 @@ class YolactNode(Node):
         self.model_path = None
         self.image_sub = None
         self.received_img = None
-        self.image_pub = self.create_publisher(Image, '/yolact_ros2/visualization', 1)
-        self.detections_pub = self.create_publisher(Detections, '/yolact_ros2/detections', 1)
+        
         self.image_vis_queue = Queue(maxsize = 1)
         self.visualization_thread = None
         self.unpause_visualization = threading.Event()
@@ -63,9 +62,14 @@ class YolactNode(Node):
             ('display_fps', self.display_fps_),
             ('score_threshold', self.score_threshold_),
             ('crop_masks', self.crop_masks_),
-            ('top_k', self.top_k_)
+            ('top_k', self.top_k_),
+            ('publish_namespace', '/yolact_ros2')
         ])
-
+        publish_ns = self.get_parameter('publish_namespace')._value
+        self.image_pub = self.create_publisher(Image, f'{publish_ns}/visualization', 1)
+        self.detections_pub = self.create_publisher(Detections, f'{publish_ns}/detections', 1)
+        self.image_pub = self.create_publisher(Image, '/yolact_ros2/visualization', 1)
+        self.detections_pub = self.create_publisher(Detections, '/yolact_ros2/detections', 1)
         self.setParams_()
 
         # Set Reconfigurable parameters Callback:
