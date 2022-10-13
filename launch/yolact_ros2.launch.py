197fc6229aa8f6430ca1205f27440326062aa1bf
fgonzalezr1998 <fergonzaramos@yahoo.es>
2022-10-14 01:52:51 +0200

    [foxy] ROS2 Foxy support

--- launch/yolact_ros2.launch.py
+++ launch/yolact_ros2.launch.py
@@ -13,15 +13,18 @@ def generate_launch_description():
     pkg_dir = get_package_share_directory('yolact_ros2')
     config_file = os.path.join(pkg_dir, 'config', params_file)
 
-    stdout_linebuf_envvar = SetEnvironmentVariable(
-        'RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
+    stdout_stdout_envvar = SetEnvironmentVariable(
+        'RCUTILS_LOGGING_USE_STDOUT', '1')
+    
+    stdout_bufstream_envvar = SetEnvironmentVariable(
+        'RCUTILS_LOGGING_BUFFERED_STREAM', '1')
 
     #Create Node:
 
     yolact_ros_node = Node(
     package='yolact_ros2',
-    node_executable='yolact_ros2_node',
-    node_name='yolact_ros2_node',
+    executable='yolact_ros2_node',
+    name='yolact_ros2_node',
     output='screen',
     parameters=[config_file]
     )
@@ -29,7 +32,8 @@ def generate_launch_description():
 
     ld = LaunchDescription()
 
-    ld.add_action(stdout_linebuf_envvar)
+    ld.add_action(stdout_stdout_envvar)
+    ld.add_action(stdout_bufstream_envvar)
     ld.add_action(yolact_ros_node)
 
     return ld
